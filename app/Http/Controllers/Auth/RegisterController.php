7b016a7953b391db3d78e018a6deda3dd821ad15
Manik <maniksharma589@gmail.com>
2021-09-14 18:24:18 +0530

    install auth in laravel

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
-use Tymon\JWTAuth\JWTAuth;
+use Tymon\JWTAuth\JWTAuth; 
 
 class RegisterController extends Controller
 {
